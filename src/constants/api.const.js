eeaddccfdd46df9f7ad123473a6890ceebf11bba
Nguyen Son <50987070+SonNVQ@users.noreply.github.com>
2023-06-28 14:17:08 +0700

    Change port

--- src/constants/api.const.js
+++ src/constants/api.const.js
@@ -2,10 +2,10 @@ import axios from 'axios'
 import { ApiService } from '../services/handlers/api.service';
 
 // base URL 
-export const baseURL = 'http://localhost:8090/api/'
+export const baseURL = 'http://localhost:8000/api/';
 
 // base axios
-export const _axios = axios.create({ baseURL: baseURL })
+export const _axios = axios.create({ baseURL: baseURL });
 
 // global API service
 export const API_SERVICE = new ApiService();
